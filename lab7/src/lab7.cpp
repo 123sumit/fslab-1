8086b4d52120545f96378352f19e399dc388f57e
123sumit <sumitcmrit@gmail.com>
2016-04-06 08:55:30 +0530

    Update lab7.cpp

--- lab7/src/lab7.cpp
+++ lab7/src/lab7.cpp
@@ -1,63 +1,138 @@
-#include <iostream>
+#include<iostream>
+#include<string.h>
+#include<stdlib.h>
 #include<fstream>
-#define max 100
+#include<sstream>
+#include<sys/types.h>
+#include<unistd.h>
 using namespace std;
-class coseq
+#define max 20
+class seqo
+{
+    string l1[max];
+    string l2[max];
+public:
+    int c1;
+    int c2;
+    void load(char flname[max],char flname1[max]);
+    void sort();
+    void match();
+};
+void seqo :: load(char flname[max],char flname1[max])
+{
+    int i=0;
+    c1=0;c2=0;
+    fstream fp1,fp2;
+    string buffer;
+    fp1.open(flname,ios::in);
+    fp2.open(flname1,ios::in);
+   // cout<<"opened file"<<endl;
+    while(!fp1.eof())
+    {
+        //cout<<"*";
+        getline(fp1,buffer);
+        l1[i]=buffer;
+        i++;
+        c1++;   
+    }
+    cout<<endl;
+    i=0;
+    while(!fp2.eof())
+    {
+       // cout<<"@";
+        getline(fp2,buffer);
+        l2[i]=buffer;
+        i++;
+        c2++;   
+    }
+   /*cout<<"value of c1 and c2 is:"<<c1<<endl<<c2<<endl;
+    cout<<endl;
+    cout<<"closed file"<<endl;*/
+     cout<<"content of l1:"<<endl;
+    for(i=0;i<c1;i++)
+        cout<<l1[i]<<endl;
+     cout<<"content of l2"<<endl;
+    for(i=0;i<c2;i++)
+        cout<<l2[i]<<endl;
+    fp1.close();
+    fp2.close();
+}
+void seqo :: sort()
+{
+    string temp;
+    int i=0,j=0;
+    for(i=0;i<c1;i++)
+    {
+     for(j=i+1;j<c1;j++)
+        {
+           if(l1[i]>l1[j])
+           {
+               temp=l1[i];
+               l1[i]=l1[j];
+               l1[j]=temp;
+           }
+        }    
+    }
+    for(i=0;i<c2;i++)
+    {
+     for(j=i+1;j<c2;j++)
+        {
+           if(l2[i]>l2[j])
+           {
+               temp=l2[i];
+               l2[i]=l2[j];
+               l2[j]=temp;
+           }
+        }    
+    }
+   cout<<"new content of l1:"<<endl;
+    for(i=0;i<c1;i++)
+        cout<<l1[i]<<endl;
+    cout<<"new content of l2"<<endl;
+    for(i=0;i<c2;i++)
+        cout<<l2[i]<<endl;
+}
+void seqo :: match()
+{
+     cout<<"matching records are :"<<endl;
+    int i=0,j=0,k=0;
+  while(i<c1 && j<c2)
 	{
-	string list1[max],list2[max];
-	int count1,count2;
-	public:
-		void load()
+		if(l1[i]==l2[j])
 		{
-			fstream fp1,fp2;
-			string buf;
-			count1=0;
-			count2=0;
-			fp1.open("/home/ise-075/workspace1/lab5/src/file1.txt");
-			while(!fp1.eof())
-				{
-					buf.erase();
-					getline(fp1,buf);
-					list1[count1++]=buf;
-					cout<<list1[count1-1]<<"  ";
-				}
-			fp1.close();
-			cout<<endl;
-			fp2.open("/home/ise-075/workspace1/lab5/src/file2.txt");
-			while(!fp2.eof())
-				{
-					buf.erase();
-					getline(fp2,buf);
-					list2[count2++]=buf;
-					cout<<list2[count2-1]<<"  ";
-				}
-			fp2.close();
-			cout<<endl;
+			cout<<l1[i]<<endl;
+			i++;
+			j++;	
 		}
-		void match()
+		if(l1[i]<l2[j])
 		{
-			int i=0,j=0;
-			cout<<"Common names are : ";
-			while(i<=count1&&j<=count2)
-				{
-				if(list1[i]==list2[j])
-					{
-					cout<<list1[i]<<"  ";
-					i++;
-					j++;
-					}
-				if(list1[i]<list2[j])
-					i++;
-				if(list1[i]>list2[j])
-					j++;
-				}
-			cout<<endl;
+			i++;
+			continue;
 		}
-	};
-int main()
-	{
-		coseq C;
-		C.load();
-		C.match();
-		return 0;
+		if(l1[i]>l2[j])
+		{
+			j++;
+			continue;
+		}
 	}
+		
+}
+int main()
+{
+    int c1=0,c2=0;
+    char fn1[max],fn2[max];
+    cout<<"enter the first filename :";
+    cin>>fn1;
+    cout<<endl;
+    cout<<"enter the second filename :";
+    cin>>fn2;
+    cout<<endl;
+    seqo s;
+   // cout<<"before loading"<<endl;
+    s.load(fn1,fn2);
+    //cout<<"done loading"<<endl;
+    s.sort();
+    //cout<<"done sorting"<<endl;
+    s.match();
+    cout<<"program terminate"<<endl;
+}
